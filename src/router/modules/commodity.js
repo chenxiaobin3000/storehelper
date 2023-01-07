df34b57d1f6613fe397c6893d3e8aa8a318fb8a8
cxb <cxb@gmail.com>
2023-01-07 20:16:14 +0800

    商品完成

--- src/router/modules/commodity.js
+++ src/router/modules/commodity.js
@@ -10,16 +10,19 @@ const commodityRouter = {
   },
   children: [{
     path: 'saleReport', component: () => import('@/views/commodity/saleReport'),
-    name: 'commodity_saleReport', meta: { title: '销售数据', roles: [304] }
+    name: 'commodity_saleReport', meta: { title: '销售数据', roles: [301] }
   }, {
     path: 'commodityList', component: () => import('@/views/commodity/commodityList'),
-    name: 'commodity_commodityList', meta: { title: '商品列表', roles: [301] }
+    name: 'commodity_commodityList', meta: { title: '商品列表', roles: [302] }
+  }, {
+    path: 'originalList', component: () => import('@/views/commodity/originalList'),
+    name: 'commodity_originalList', meta: { title: '原料列表', roles: [303] }
   }, {
     path: 'setCategory', component: () => import('@/views/commodity/setCategory'),
-    name: 'commodity_setCategory', meta: { title: '品类设置', roles: [302] }
+    name: 'commodity_setCategory', meta: { title: '品类设置', roles: [304] }
   }, {
     path: 'attributeList', component: () => import('@/views/commodity/attributeList'),
-    name: 'system_attributeList', meta: { title: '属性管理', roles: [303] }
+    name: 'system_attributeList', meta: { title: '属性管理', roles: [305] }
   }]
 }
 
--- src/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export function showPrice(price) {
-  return (price / 100) + '.' + (price % 100)
-}
